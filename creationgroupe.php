1cc5a0bbca75ac511076a51e54008cd006176351
augustineo <persia.martouf@gmail.com>
2013-05-06 14:47:05 +0200

    maj creatgroupe

--- creationgroupe.php
+++ creationgroupe.php
@@ -18,15 +18,32 @@ else
 	if(isset($_POST['groupe'])&&($_POST['groupe']=='sub'))
 	{
 		// ajout groupe
-		$res=$bdd->query("INSERT INTO GROUPE (ID_GROUPE,ID_USERS,NOM) VALUES(".$_SESSION['uid'].",".$_POST['nom'].")");
+		$res=$bdd->query("INSERT INTO GROUPE (ID_USERS,NOM) VALUES(".$_SESSION['uid'].",'".$_POST['nom']."')");
 		$idgroupe=$bdd->lastInsertId();
 		
 		//ajout users
 		$indice=1;
-		while(isset($_POST['user'.$indice))
+		while(isset($_POST['user'.$indice]))
 		{
-		$res=$bdd->query("INSERT INTO FONT_PARTIE (ID_USERS,ID_GROUPE) VALUES('".$_POST['user'.$ite]."',".$idgroupe." )");
-		$indice++;
+			// verifier avant le premier insert
+			$select=$bdd->query("SELECT ID_USERS FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."');");
+			if(mysql_num_rows($select)==0){
+				echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." n'est pas enregistré. SI vous avez fait une erreur, ajoutez cet utilisateur sur la page du groupe</h2>";
+			}
+		
+			//recupere id associé au mail
+			//si pas de ligne echo de l'adresse mail qui marche pas, plus verif champ valid null quand mis en place donc echo différent
+			else {
+			// $select2=$bdd->query("SELECT VALID FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."');");
+			/*if($select2!=NULL)
+			{
+				echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." n'a pas encore validé son compte, ajoutez le plus tard.</h2>";
+			}*/
+			//else{
+			$res=$bdd->query("INSERT INTO FONT_PARTIE (ID_USERS,ID_GROUPE) VALUES(".$select.",".$idgroupe." )");
+			//}
+			}
+			$indice++;
 		}
 	}
 
@@ -38,26 +55,23 @@ else
 		
 		<form action="creationgroupe.php" method="post">
 		<input type="hidden" name="groupe" value="sub">
-		<fieldset>
-		<legend>Nom</legend>
 		<label for="nom">Nom : </label>
 		<input type="text" id="nom" name="nom"/> <br/>
-		</fieldset>
 		<fieldset id="ajoutuser">
 		<legend> Ajout d'utilisateurs </legend>
 		<script type="text/javascript">
-			var nbu=2;
+			var nbu=1;
 			function adduser()
 			{
 				nbu++;
 				var q=document.getElementById("ajoutuser");
 				var form=document.createElement("div");
-				var html="<label for=\"user"+nbu+"\">Id de l'utilisateur "+nbu"+</label><input type=\"text\" id=\"user"+nbu+"\" name=\"user"+nbu+"\"/><br/>"+"style=\"margin-left:2%;\">";
+				var html="<label for=\"user"+nbu+"\">Mail de l'utilisateur "+nbu+" :</label><input type=\"text\" id=\"user"+nbu+"\" name=\"user"+nbu+"\"/><br/>";
 				form.innerHTML=html;
 				q.appendChild(form);
 			}
 		</script>
-		<label for="user1"> Id de l'utilisateur 1: </legend>
+		<label for="user1"> Mail l'utilisateur 1: </legend>
 		<input type="text" id="user1" name="user1"/><br />
 		</fieldset>
 		<input type="button" value="Ajouter un utilisateur" onclick="adduser();"/><br />
