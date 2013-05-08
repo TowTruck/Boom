57e3bcc39058f2ab41a665569338250566ea2359
augustineo <persia.martouf@gmail.com>
2013-05-08 17:09:55 +0200

    creationgroup sans public

--- creationgroupe.php
+++ creationgroupe.php
@@ -17,36 +17,44 @@ else
 	$user=$_SESSION['login'];
 	if(isset($_POST['groupe'])&&($_POST['groupe']=='sub'))
 	{
-		// ajout groupe
-		$res=$bdd->query("INSERT INTO GROUPE (ID_USERS,NOM) VALUES(".$_SESSION['uid'].",'".$_POST['nom']."')");
-		$idgroupe=$bdd->lastInsertId();
-		
-		//ajout users
-		$indice=1;
-		while(isset($_POST['user'.$indice]))
+		//si public (peut importe la casse)
+		if(strcasecmp($_POST['nom'], "public") == 0)
 		{
-			// verifier avant le premier insert
-			$select=$bdd->query("SELECT COUNT(*) AS COMPT FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."' AND VALIDE IS NULL ;");
-			$select->setFetchMode(PDO::FETCH_OBJ);
-			$ligne=$select->fetch();
-			if($ligne->COMPT == 0){
-				echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." n'est pas enregistr&eacute; ou non valid&eacute;. SI vous avez fait une erreur, ajoutez cet utilisateur sur la page du groupe</h2>";
+			echo "<h2>Vous ne pouvez pas utiliser ce nom pour un groupe </h2>";
+		}
+		else
+		{
+			// ajout groupe
+			$res=$bdd->query("INSERT INTO GROUPE (ID_USERS,NOM) VALUES(".$_SESSION['uid'].",'".$_POST['nom']."')");
+			$idgroupe=$bdd->lastInsertId();
+		
+			//ajout users
+			$indice=1;
+			while(isset($_POST['user'.$indice]))
+			{
+				// verifier avant le premier insert
+				$select=$bdd->query("SELECT COUNT(*) AS COMPT FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."' AND VALIDE IS NULL ;");
+				$select->setFetchMode(PDO::FETCH_OBJ);
+				$ligne=$select->fetch();
+				if($ligne->COMPT == 0){
+					echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." n'est pas enregistr&eacute; ou non valid&eacute;. Si vous avez fait une erreur, ajoutez cet utilisateur sur la page du groupe</h2>";
 				
-			}
+				}
 			
 		
-			//recupere id associé au mail
-			//si pas de ligne echo de l'adresse mail qui marche pas, plus verif champ valid null quand mis en place donc echo différent
-			else {
-			
-			$ident = $bdd->query("SELECT ID_USERS AS ID FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."';");
-			$ident->setFetchMode(PDO::FETCH_OBJ);
-			$ide=$ident->fetch();
-			$res=$bdd->query("INSERT INTO FONT_PARTIE (ID_USERS,ID_GROUPE) VALUES(".$ide->ID.",".$idgroupe." )");
-			echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." a &eacute;t&eacute; ajout&eacute; au groupe.</h2><br />";
+				//recupere id associé au mail
+				//si pas de ligne echo de l'adresse mail qui marche pas, plus verif champ valid null quand mis en place donc echo différent
+				else {
+				
+				$ident = $bdd->query("SELECT ID_USERS AS ID FROM USERS WHERE MAIL= '".$_POST['user'.$indice]."';");
+				$ident->setFetchMode(PDO::FETCH_OBJ);
+				$ide=$ident->fetch();
+				$res=$bdd->query("INSERT INTO FONT_PARTIE (ID_USERS,ID_GROUPE) VALUES(".$ide->ID.",".$idgroupe." )");
+				echo "<h2>L'utilisateur ayant le mail ".$_POST['user'.$indice]." a &eacute;t&eacute; ajout&eacute; au groupe.</h2><br />";
+				}
+				
+				$indice++;
 			}
-			
-			$indice++;
 		}
 	}
 
