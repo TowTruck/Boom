be1c8813c97f8cf7751cabeaa125c50b96bda8fa
Nofriends <anthonyroux65@hotmail.com>
2013-05-08 16:50:31 +0300

    Create recherche.php

---
+++ recherche.php
@@ -0,0 +1,39 @@
+<?php
+require('top.php');
+?>
+
+<div id="centre">
+
+<?php 
+if(empty($_POST['recherche']))
+{
+  echo "Veuillez saisir une recherche !";
+}
+else
+{
+	$recherche = $_POST['recherche'];
+	$nb = 0;	
+	
+	$req = $bdd->query("SELECT * FROM QCM WHERE INTITULE = %".$recherche."%;");
+	$req->setFetchMode(PDO::FETCH_OBJ);
+	while( $res = $req->fetch() )
+	{
+		echo '<a href="qcm.php?id='.$res->ID_QCM.'">'.$res->INTITULE.'</a><br>';
+		$nb++;
+	}
+	
+	if($nb == 0)
+	{
+		echo "Aucun résultat pour votre recherche";
+	}
+	else
+	{
+		echo $nb." résultat(s) trouvé(s)";
+	}
+}
+?>
+</div>
+
+<?php
+require('bottom.php');
+?>
