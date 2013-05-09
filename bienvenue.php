46d55fdad472906ec9894224f9153aa3a0477b08
augustineo <persia.martouf@gmail.com>
2013-05-09 18:06:03 +0200

    Update bienvenue

--- bienvenue.php
+++ bienvenue.php
@@ -5,21 +5,21 @@ require('top.php');
 <?php
   if(isAuth())
 	{	
-		echo " <h3>Bienvenu sur le site de QCM de l'université de bourgogne ! <br/><br/><br/></h3>";
+		echo " <h3>Bienvenue sur le site de QCM de l'universit&eacute; de Bourgogne ! <br/><br/><br/></h3>";
 		
-		echo "	<p>Accéder dès maintenant à vos résultats et aux nouveaux QCM mis en ligne.</p>";
+		echo "	<p>Acc&eacute;der d&egrave;s maintenant &agrave; vos r&eacute;sultats et aux nouveaux QCM mis en ligne.</p>";
 	}
   else
 	{
-		echo"<h3>Bienvenu sur le site de QCM de l'université de bourgogne ! <br/><br/><br/></h3>";
+		echo"<h3>Bienvenu sur le site de QCM de l'universit&eacute;de bourgogne ! <br/><br/><br/></h3>";
 		
-		echo"<p>Vous trouverez sur ce site des QCM relatifs à toutes les matières enseignées sur le campus.<br/><br/>
+		echo"<p>Vous trouverez sur ce site des QCM relatifs à toutes les mati&egrave;res enseign&eacute;es sur le campus.<br/><br/>
 		
-		Ce site est à but pédagogique, principalement pour tester ses connaissances.<br/>
-		Cependant, les professeurs utiliseront ce site dans le but de d'évaluer vos connaissances.<br/><br/>
+		Ce site est &agrave; but p&eacute;dagogique, principalement pour tester ses connaissances.<br/>
+		Cependant, les professeurs utiliseront ce site dans le but de d'&eacute;valuer vos connaissances.<br/><br/>
 		
-		Inscrivez-vous et connectez-vous afin d'avoir accès à toutes les rubriques !<br/>
-		Les QCM publics sont à la dispositions de tous les utilisateurs.</p>";
+		Inscrivez-vous et connectez-vous afin d'avoir acc&egrave;s à toutes les rubriques !<br/>
+		Les QCM publics sont &agrave; la dispositions de tous les utilisateurs.</p>";
 	}
 ?>
 </div>
