fa4b9f07190b082aa17d9732e4ac5b6e640eb223
Yanis <kloon.me@free.fr>
2013-05-09 19:55:43 +0200

    Maj administration

--- administration.php
+++ administration.php
@@ -2,7 +2,18 @@
 require('top.php');
 ?>
 <div id="centre">
-
+<script type="text/javascript">
+function affiche_div(bouton, id) { 
+  var div = document.getElementById(id); 
+  if(div.style.display=="none") { 
+    div.style.display = "block";
+    bouton.innerHTML = "Cacher"; 
+  } else { 
+    div.style.display = "none"; 
+    bouton.innerHTML = "Afficher"; 
+  }
+}
+</script>
 <?php
 
 //validation des qcms
@@ -65,23 +76,55 @@ else
 {
 	echo'<h2> QCM en attente de validation </h2>';
 	echo '<form action="administration.php" method="post">';
-			echo '<input type="hidden" name="validation" value="val">';
+			echo '<input type="hidden" name="validation" value="val"/>';
 			echo "<table>";
-			echo "<tr><td> Id du createur </td> <td> Intitul&eacute; du QCM </td> <td> Valider le QCM? </td><td> Supprimer le QCM </td></tr>";
+			echo "<tr><td> Id du createur </td> <td> Intitul&eacute; du QCM </td> <td> Valider le QCM? </td><td> Supprimer le QCM </td><td> Visualiser </td></tr>";
 			$select1 = $bdd->query("SELECT * FROM QCM WHERE ID_QCM IN (SELECT ID_QCM FROM LIAISON WHERE ID_GROUPE = 0);");
 			$select1->setFetchMode(PDO::FETCH_OBJ);
 			while( $enregistrement1 = $select1->fetch())
 			{
 				
-				echo '<tr><td>'.$enregistrement1->ID_USERS.'<td><a href="qcm.php?id='.$enregistrement1->ID_QCM.'" target="_blank">'.$enregistrement1->INTITULE.'</a></td><td> <input type="radio" id="val'.$enregistrement1->ID_QCM.'" name="val'.$enregistrement1->ID_QCM.'" value="validation" /></td><td> <input type="radio" id="sup'.$enregistrement1->ID_QCM.'" name="val'.$enregistrement1->ID_QCM.'" value="suppression"/></td></tr>';
-				
+				echo '<tr><td>'.$enregistrement1->ID_USERS.'</td><td><a href="qcm.php?id='.$enregistrement1->ID_QCM.'" target="_blank">'.$enregistrement1->INTITULE.'</a></td><td> <input type="radio" id="val'.$enregistrement1->ID_QCM.'" name="val'.$enregistrement1->ID_QCM.'" value="validation" /></td><td> <input type="radio" id="sup'.$enregistrement1->ID_QCM.'" name="val'.$enregistrement1->ID_QCM.'" value="suppression"/></td><td><button type="button" onclick="affiche_div(this,\''.$enregistrement1->ID_QCM.'\');">Afficher</button></td></tr>';
 			}
-			echo"</table>";
+			echo "</table>";
 			echo '<input type="submit" value="Valider/Supprimer"/>';
 			echo "</form>";
-
-	
-	echo'<h2> Gestion des utilisateurs </h2>';
+			
+			echo "<br/><br/><div><h2>Previsualisation</h2>";
+			$select11 = $bdd->query("SELECT ID_QCM FROM LIAISON WHERE ID_GROUPE = 0");
+			$select11->setFetchMode(PDO::FETCH_OBJ);
+			while( $enregistrement1 = $select11->fetch())
+			{
+			echo "<div id=\"".$enregistrement1->ID_QCM."\" style=\"display:none;\">";
+			$select = $bdd->query("SELECT * FROM QCM WHERE ID_QCM = ".$enregistrement1->ID_QCM."");
+			$select->setFetchMode(PDO::FETCH_OBJ);
+			while( $enregistrement = $select->fetch())
+			{
+			echo "Intitule : ".$enregistrement->INTITULE."<br/>";
+			echo "Type : ".$enregistrement->TYPES."<br/>";
+			echo "Note : ".$enregistrement->NOTE."<br/><br/>";
+			
+			$select12 = $bdd->query("SELECT * FROM QUESTIONS WHERE ID_QCM=".$enregistrement1->ID_QCM." ORDER BY ID_QUESTIONS ASC");
+			$select12->setFetchMode(PDO::FETCH_OBJ);
+			while( $enregistrement2 = $select12->fetch())
+			{
+			
+			echo "Question : ".$enregistrement2->INTITULE."<br/>Reponses possibles : <br/>";
+			
+			$select13 = $bdd->query("SELECT * FROM REPONSES WHERE ID_QUESTIONS=".$enregistrement2->ID_QUESTIONS."  ORDER BY ID_REPONSES ASC");
+			$select13->setFetchMode(PDO::FETCH_OBJ);
+			while( $enregistrement3 = $select13->fetch())
+			{
+			echo $enregistrement3->NOM."           Valeur : ".$enregistrement3->VALEUR."<br/>";
+			}
+			echo "<br/><br/>";
+			}
+			}
+			echo "</div>";
+			}
+			echo "</div>";
+			
+	echo'<br/><br/><h2> Gestion des utilisateurs </h2>';
 	echo '<form action="administration.php" method="post">';
 			echo '<input type="hidden" name="suppr" value="del">';
 			echo "<table>";
