ad1485fd05a8003bd7a3f6aae512dbc43d334838
Yanis <kloon.me@free.fr>
2013-05-11 02:49:29 +0200

    Mis a jour recherche

--- recherche.php
+++ recherche.php
@@ -14,13 +14,27 @@ else
 	$recherche = $_POST['recherche'];
 	$nb = 0;	
 	
-	$req = $bdd->query("SELECT * FROM QCM WHERE INTITULE = %".$recherche."%;");
+	//Affichage des qcm publics
+	$req = $bdd->query("SELECT QCM.ID_QCM,QCM.INTITULE,LIAISON.ID_GROUPE FROM QCM,LIAISON WHERE QCM.INTITULE LIKE '%".$recherche."%' AND LIAISON.ID_GROUPE=1 AND QCM.ID_QCM=LIAISON.ID_QCM;");
 	$req->setFetchMode(PDO::FETCH_OBJ);
 	while( $res = $req->fetch() )
 	{
 		echo '<a href="qcm.php?id='.$res->ID_QCM.'">'.$res->INTITULE.'</a><br>';
 		$nb++;
 	}
+	
+	//Si authentifié on ajoute les qcm reservé a son groupe
+	if(isAuth())
+	{
+	$req = $bdd->query("SELECT QCM.ID_QCM,QCM.INTITULE,LIAISON.ID_GROUPE,FONT_PARTIE.ID_USERS,FONT_PARTIE.ID_GROUPE FROM QCM,LIAISON,FONT_PARTIE WHERE QCM.INTITULE LIKE '%".$recherche."%' AND QCM.ID_QCM=LIAISON.ID_QCM AND LIAISON.ID_GROUPE=FONT_PARTIE.ID_GROUPE AND FONT_PARTIE.ID_USERS=".$_SESSION['uid'].";");
+	$req->setFetchMode(PDO::FETCH_OBJ);
+	while( $res = $req->fetch() )
+	{
+		echo '<a href="qcm.php?id='.$res->ID_QCM.'">'.$res->INTITULE.'</a><br>';
+		$nb++;
+	}
+	}
+	
 	
 	if($nb == 0)
 	{
